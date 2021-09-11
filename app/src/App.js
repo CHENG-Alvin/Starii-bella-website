3f3bf5caad406b2e536c1002c2b8f5d2767be8fb
Alvincheng137 <chengalvin333@gmail.com>
2021-09-11 15:54:37 +1000

    Project setup

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,23 @@
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Videos from "./Videos.js";
+import Shop from "./Shop.js";
+import Home from "./Home.js";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <p>hello world</p>
+      {/*Router*/}
+      <Router>
+        <Switch>
+          {/*Home Route*/}
+          <Route path="/" exact component={Home} />
+          {/*Shop Route*/}
+          <Route path="/shop" exact component={Shop} />
+          {/*Video Route*/}
+          <Route path="/video" exact component={Videos} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ app/src/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+import Navbar from "";
+const Home = () => {
+  return <div></div>;
+};
+
+export default Home;
