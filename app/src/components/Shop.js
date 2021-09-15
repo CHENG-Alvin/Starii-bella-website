39054f98e56f22b14cd24c1b7d32852794eca8a9
Alvincheng137 <chengalvin333@gmail.com>
2021-09-15 21:23:03 +1000

    Shop page complete

--- app/src/components/Shop.js
+++ app/src/components/Shop.js
@@ -1,7 +1,41 @@
+//Importing packages
 import React from "react";
+import { Link } from "react-router-dom";
 
+//Shop page
 const Shop = () => {
-  return <div></div>;
+  //Render component
+  return (
+    <div>
+      {/*On screen text*/}
+      <h1
+        style={{
+          fontFamily: "Brush Script MT, Brush Script Std, cursive",
+          color: "black",
+          fontSize: "60px",
+          paddingTop: "20vh",
+        }}
+      >
+        Comming soon...
+      </h1>
+      {/*Back to home link*/}
+      <Link
+        style={{
+          textDecoration: "none",
+          color: "black",
+          backgroundColor: "black",
+          padding: "1rem",
+          borderRadius: ".5rem",
+          fontFamily: "Poppins, sans-serif",
+          color: "#fff",
+        }}
+        to="/"
+      >
+        Back to home
+      </Link>
+    </div>
+  );
 };
 
+//Export component
 export default Shop;
