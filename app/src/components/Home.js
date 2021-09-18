954fb2ab3856d267bbe61f2ed0ed78774fcef41a
Alvincheng137 <chengalvin333@gmail.com>
2021-09-18 12:03:05 +1000

    Modified home page

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -1,11 +1,55 @@
-import React from "react";
+//Import packages
+import React, { useEffect } from "react";
 import Banner from "./Banner.js";
+import Links from "./Links.js";
+import youtubeLogo from "../content/youtubeLogo.png";
+import discordLogo from "../content/discordLogo.jpg";
+import "./styles/Home.css";
+import Aos from "aos";
+import "aos/dist/aos.css";
+
+//Home component
 const Home = () => {
+  //Aos Init
+  useEffect(() => {
+    Aos.init({ duration: 200 });
+  }, []);
+
+  //Render component
   return (
     <div>
-      <Banner />
+      {/*Upper app*/}
+      <div id="upperApp">
+        {/*Banner*/}
+        <Banner />
+      </div>
+      {/*Lower app*/}
+      <div id="lowerApp">
+        {/*Links*/}
+        <div id="links" data-aos="fade-up">
+          {/*Youtube link*/}
+          <Links
+            id="mediaLinks"
+            imgSrc={youtubeLogo}
+            btnText="Watch more!"
+            tittle="More in depth!"
+            link="https://www.youtube.com/channel/UCXc3hzORGb-osun3RcY0oOw"
+            iconPadding="8vh"
+          />
+          {/*Discord link*/}
+          <Links
+            id="mediaLinks"
+            imgSrc={discordLogo}
+            btnText="Join now!"
+            tittle="Let's chat!"
+            link="https://discord.gg/XFTtKjwr73"
+            iconPadding="8vh"
+          />
+        </div>
+      </div>
     </div>
   );
 };
 
+//Export component
 export default Home;
