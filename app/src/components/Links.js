69d5fac9a726180f31db4bd6e0bc77d3255837d3
Alvincheng137 <chengalvin333@gmail.com>
2021-09-18 12:03:23 +1000

    Created links

---
+++ app/src/components/Links.js
@@ -0,0 +1,40 @@
+//Importing packages
+import React from "react";
+import MediaLink from "./MediaLink.js";
+import "./styles/Link.css";
+
+//Link component
+const Links = (props) => {
+  //Render component
+  return (
+    <div id="whole">
+      {/*Tittle*/}
+      <h1 id="tittle">{props.tittle}</h1>
+
+      {/*Link*/}
+      <MediaLink
+        id="mediaLinks"
+        textColor="#4b95df"
+        backgroundColor="black"
+        text={props.btnText}
+        backgroundColor="white"
+        borderRad="60px 60px 60px 60px"
+        borderColor="#4b95df"
+        link={props.link}
+      />
+      {/*Image*/}
+      <img
+        id="icon"
+        src={props.imgSrc}
+        style={{
+          objectFit: "contain",
+          borderRadius: props.borderRad || "0px",
+          padding: props.iconPadding || "0px",
+        }}
+      ></img>
+    </div>
+  );
+};
+
+//Export component
+export default Links;
