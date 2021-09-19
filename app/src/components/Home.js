35e14812935184622e71bb3ee370bdaf4cdf6771
Alvincheng137 <chengalvin333@gmail.com>
2021-09-19 18:05:16 +1000

    Preview build

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -12,7 +12,12 @@ import "aos/dist/aos.css";
 const Home = () => {
   //Aos Init
   useEffect(() => {
-    Aos.init({ duration: 200 });
+    Aos.init({ duration: 1500 });
+    setTimeout(() => {
+      alert(
+        "Welcome to the Starii bella website! This is the preview version this means, there might be some bugs or issues in the website. Further more the content and shop page is still not completed.\nThank you!"
+      );
+    }, 1000);
   }, []);
 
   //Render component
@@ -26,7 +31,7 @@ const Home = () => {
       {/*Lower app*/}
       <div id="lowerApp">
         {/*Links*/}
-        <div id="links" data-aos="fade-up">
+        <div id="links" data-aos="fade-up" data-aos-easing="ease-in-out">
           {/*Youtube link*/}
           <Links
             id="mediaLinks"
