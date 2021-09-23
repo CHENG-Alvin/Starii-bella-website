a545bbdb6d7ef7a53a3712fe4688cc895b0bc022
Alvincheng137 <chengalvin333@gmail.com>
2021-09-23 11:44:17 +1000

    Created a Message component

---
+++ app/src/components/Message.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Message = () => {
+  return (
+    <div>
+      <h1 style={{ fontFamily: "Bebas Neue, cursive", paddingTop: "15vw" }}>
+        A message from Starii Bella:
+      </h1>
+      <h1
+        style={{
+          fontFamily: "Bebas Neue, cursive",
+          padding: "0vw 10vw 12vw 10vw",
+        }}
+      >
+        Hi lovelies! I hope you like my videos I’m gonna upload and my dream is
+        to heat 100 subscribers this year! If you are ogs, then you’ll know my
+        old channel was called gaming with Bella- games, but now it’s called
+        Simply Metallic! My name is Bella and I hope you love my video! Also
+        welcome to Starii Bella ୨୧
+      </h1>
+    </div>
+  );
+};
+
+export default Message;
