ce0e2e521999612ca70402367e2cc1b57b75fbf2
Alvincheng137 <chengalvin333@gmail.com>
2021-09-23 11:46:46 +1000

    Modified Banner and SUb count components

---
+++ app/src/components/HomeBanner.js
@@ -0,0 +1,47 @@
+//Importing packages
+import React from "react";
+import NavBar from "./NavBar.js";
+import bannerVideo from "../content/bannerVideo.mp4";
+import bannerImage from "../content/icon.jpg";
+import "./styles/HomeBanner.css";
+import MediaLink from "./MediaLink.js";
+
+//Banner component
+const HomeBanner = () => {
+  //Render component
+  return (
+    <div>
+      {/*Navbar*/}
+      <NavBar
+        backgroundColor="transparent"
+        color="white"
+        id="bannerNavbar"
+      ></NavBar>
+      {/*Banner Content*/}
+      <div id="bannerContent">
+        {/*Channel Icon*/}
+        <img id="bannerIcon" src={bannerImage} alt="Error" />
+        <div id="bannerText">
+          {/*Channel name*/}
+          <h1 id="bannerChannelName">Starii Bella.</h1>
+          {/*Channel discription*/}
+          <p id="bannerDiscription">Roblox edits, Gameplay and more!</p>
+          {/*Subscribe link*/}
+          <MediaLink
+            borderColor="#ffff"
+            textColor="black"
+            borderRad="0.5rem"
+            backgroundColor="#ffff"
+            text="Subscribe!"
+            link="https://www.youtube.com/channel/UCXc3hzORGb-osun3RcY0oOw"
+          />
+        </div>
+      </div>
+      {/*Background Video*/}
+      <video src={bannerVideo} autoPlay loop muted id="bannerVideo" />
+    </div>
+  );
+};
+
+//Export component
+export default HomeBanner;
