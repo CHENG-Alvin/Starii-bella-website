75bc150499be1f6b2d5399aa967f28a0eda14e95
AlvinC888 <chengalvin333@gmail.com>
2021-09-30 09:29:57 +1000

    Update Subscriber count component

--- app/src/components/SubCount.js
+++ app/src/components/SubCount.js
@@ -14,7 +14,6 @@ const SubCount = () => {
     requests
       .get(endPoint.subCount)
       .then((response) => {
-        //! Error at Link 18
         setSubCount(response.data.items[0].statistics.subscriberCount);
       })
       .catch((err) => console.error(err));
