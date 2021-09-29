61f19cc093b7ca34577c189907fb5562e06f02ac
Alvin Cheng <88267875+AlvinC888@users.noreply.github.com>
2021-09-29 19:19:04 +1000

    Added curves

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -34,15 +34,43 @@ const Home = () => {
       {/*Lower app*/}
       <div id="lowerApp">
         {/*Message*/}
-        <div id="message" data-aos="fade-up">
+        <div id="message" data-aos="fade-down">
           <Message />
         </div>
         {/*Comments*/}
-        <div id="comments" style={{ margin: "6vw 0vw 6vw 0vw" }}>
+        <div
+          id="comments"
+          data-aos="fade-down"
+          style={{ margin: "6vw 0vw 6vw 0vw" }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1440 320"
+            style={{ position: "relative", top: "2vw" }}
+          >
+            <path
+              fill="#4b95df"
+              fillOpacity="1"
+              d="M0,0L48,53.3C96,107,192,213,288,240C384,267,480,213,576,213.3C672,213,768,267,864,266.7C960,267,1056,213,1152,197.3C1248,181,1344,203,1392,213.3L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+            ></path>
+          </svg>
+
           <Comments />
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1440 320"
+            style={{ position: "relative", bottom: "3vw" }}
+          >
+            <path
+              fill="#4b95df"
+              fillOpacity="1"
+              d="M0,0L48,53.3C96,107,192,213,288,240C384,267,480,213,576,213.3C672,213,768,267,864,266.7C960,267,1056,213,1152,197.3C1248,181,1344,203,1392,213.3L1440,224L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+            ></path>
+          </svg>
         </div>
         {/*Links*/}
-        <div id="links" data-aos="fade-up" data-aos-easing="ease-in-out">
+        <div id="links" data-aos="fade-down" data-aos-easing="ease-in-out">
           {/*Youtube link*/}
           <Links
             id="mediaLinks"
@@ -63,7 +91,18 @@ const Home = () => {
           />
         </div>
         <div id="subCounter" data-aos="fade-up">
-          <SubCount></SubCount>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1440 320"
+            style={{ position: "relative", top: "2vw" }}
+          >
+            <path
+              fill="#4b95df"
+              fillOpacity="1"
+              d="M0,0L48,53.3C96,107,192,213,288,240C384,267,480,213,576,213.3C672,213,768,267,864,266.7C960,267,1056,213,1152,197.3C1248,181,1344,203,1392,213.3L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+            ></path>
+          </svg>
+          <SubCount />
         </div>
       </div>
     </div>
