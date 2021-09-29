2d3dff013ed7fd29d0d52746cf4b7f330691e31f
Alvin Cheng <88267875+AlvinC888@users.noreply.github.com>
2021-09-29 19:19:51 +1000

    Update Shop.js

--- app/src/components/Shop.js
+++ app/src/components/Shop.js
@@ -1,38 +1,13 @@
 //Importing packages
 import React from "react";
-import { Link } from "react-router-dom";
+import CommingSoon from "./CommingSoon.js";
 
 //Shop page
 const Shop = () => {
   //Render component
   return (
     <div>
-      {/*On screen text*/}
-      <h1
-        style={{
-          fontFamily: "Brush Script MT, Brush Script Std, cursive",
-          color: "black",
-          fontSize: "60px",
-          paddingTop: "20vh",
-        }}
-      >
-        Comming soon...
-      </h1>
-      {/*Back to home link*/}
-      <Link
-        style={{
-          textDecoration: "none",
-
-          backgroundColor: "black",
-          padding: "1rem",
-          borderRadius: ".5rem",
-          fontFamily: "Poppins, sans-serif",
-          color: "#fff",
-        }}
-        to="/"
-      >
-        Back to home
-      </Link>
+      <CommingSoon />
     </div>
   );
 };
