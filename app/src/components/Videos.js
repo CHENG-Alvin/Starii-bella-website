dc94f8e874eb08913d6ceb6e2e563554245fdb0b
Alvin Cheng <88267875+AlvinC888@users.noreply.github.com>
2021-09-29 19:24:09 +1000

    Changed files and included coming soon component

--- app/src/components/Videos.js
+++ app/src/components/Videos.js
@@ -1,15 +1,23 @@
 //Importing packages
 import React from "react";
+<<<<<<< Updated upstream
 import VideoDisplay from "./VideoDisplay.js";
 import VideoBanner from "./VideoBanner.js";
+=======
+import CommingSoon from "./CommingSoon.js";
+>>>>>>> Stashed changes
 
 //Shop page
 const Videos = () => {
   //Render component
   return (
     <div>
+<<<<<<< Updated upstream
       <VideoBanner />
       <VideoDisplay />
+=======
+      <CommingSoon />
+>>>>>>> Stashed changes
     </div>
   );
 };
