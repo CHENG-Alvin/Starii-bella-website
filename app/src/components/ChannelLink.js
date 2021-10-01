4602cba1bbf0f6d4a2c9ea9e175251be19fa5f97
AlvinC888 <chengalvin333@gmail.com>
2021-10-01 12:54:15 +1000

    Created Channel Link component

---
+++ app/src/components/ChannelLink.js
@@ -0,0 +1,37 @@
+//Importing packages
+import React from "react";
+import MediaLink from "./MediaLink.js";
+
+//ChannelLink component
+const ChannelLink = () => {
+  return (
+    <div style={{ backgroundColor: "#4b95df", padding: "9vw" }}>
+      {/*Tittle*/}
+      <strong
+        style={{
+          fontSize: "3vw",
+          fontFamily: "Bebas Neue, cursive",
+          color: "#fff",
+        }}
+      >
+        SUBSCRIBE TO SWAGGYDINOS
+      </strong>
+      {/*Link*/}
+      <div style={{ padding: "3vw" }}>
+        <MediaLink
+          id="mediaLinks"
+          textColor="white"
+          text="SUBSCRIBE TO SWAGGYDINOS"
+          backgroundColor="red"
+          borderRad="0px"
+          borderColor="#4b95df"
+          link="https://www.youtube.com/channel/UC3wdFUo2NqeQxg0_u6-M8QQ"
+          fontFamily="Bebas Neue, cursive"
+        />
+      </div>
+    </div>
+  );
+};
+
+///Export component
+export default ChannelLink;
--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -8,9 +8,10 @@ import Comments from "./Comments.js";
 import Aos from "aos";
 import SubCount from "./SubCount.js";
 import Message from "./Message.js";
+import ChannelLink from "./ChannelLink.js";
 import "aos/dist/aos.css";
-
 import "./styles/Home.css";
+
 //Home component
 const Home = () => {
   //Aos Init
@@ -37,12 +38,9 @@ const Home = () => {
         <div id="message" data-aos="fade-down">
           <Message />
         </div>
-        {/*Comments*/}
-        <div
-          id="comments"
-          data-aos="fade-down"
-          style={{ margin: "6vw 0vw 6vw 0vw" }}
-        >
+        {/*Container*/}
+        <div data-aos="fade-down" style={{ margin: "6vw 0vw 6vw 0vw" }}>
+          {/*SVG curves*/}
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 1440 320"
@@ -54,20 +52,25 @@ const Home = () => {
               d="M0,0L48,53.3C96,107,192,213,288,240C384,267,480,213,576,213.3C672,213,768,267,864,266.7C960,267,1056,213,1152,197.3C1248,181,1344,203,1392,213.3L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
             ></path>
           </svg>
+          {/*Channel link*/}
+          <ChannelLink />
+          {/*Comments*/}
+          <div id="comments">
+            <Comments />
 
-          <Comments />
-
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 1440 320"
-            style={{ position: "relative", bottom: "3vw" }}
-          >
-            <path
-              fill="#4b95df"
-              fillOpacity="1"
-              d="M0,0L48,53.3C96,107,192,213,288,240C384,267,480,213,576,213.3C672,213,768,267,864,266.7C960,267,1056,213,1152,197.3C1248,181,1344,203,1392,213.3L1440,224L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
-            ></path>
-          </svg>
+            {/*SVG curves*/}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 1440 320"
+              style={{ position: "relative", bottom: "3vw" }}
+            >
+              <path
+                fill="#4b95df"
+                fillOpacity="1"
+                d="M0,0L48,53.3C96,107,192,213,288,240C384,267,480,213,576,213.3C672,213,768,267,864,266.7C960,267,1056,213,1152,197.3C1248,181,1344,203,1392,213.3L1440,224L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+              ></path>
+            </svg>
+          </div>
         </div>
         {/*Links*/}
         <div id="links" data-aos="fade-down" data-aos-easing="ease-in-out">
@@ -91,6 +94,7 @@ const Home = () => {
           />
         </div>
         <div id="subCounter" data-aos="fade-up">
+          {/*SVG curves*/}
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 1440 320"
@@ -102,6 +106,7 @@ const Home = () => {
               d="M0,0L48,53.3C96,107,192,213,288,240C384,267,480,213,576,213.3C672,213,768,267,864,266.7C960,267,1056,213,1152,197.3C1248,181,1344,203,1392,213.3L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
             ></path>
           </svg>
+          {/*Subscriber count*/}
           <SubCount />
         </div>
       </div>
